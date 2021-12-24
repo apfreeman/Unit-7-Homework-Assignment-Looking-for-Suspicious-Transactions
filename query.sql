c16a869b6b94a453eecf559f9795e64da021291a
Adam Freeman <adam.freeman@outlook.com.au>
2021-12-24 12:35:26 +1000

    ver 0.6 - Draft(Data Engineering)

--- query.sql
+++ query.sql
@@ -1,19 +1,19 @@
---- Check data after import
+--- Check card_holder table data after import
 SELECT *
 FROM card_holder
 
---- Check data after import
+--- Check credit_card table data after import
 SELECT *
 FROM credit_card
 
---- Check data after import
+--- Check merchant table data after import
 SELECT *
 FROM merchant
 
---- Check data after import
+--- Check merchant_category table data after import
 SELECT *
 FROM merchant_category
 
---- Check data after import
+--- Check transaction table data after import
 SELECT *
 FROM transaction
